c6b2acd666f50bfade2d4f6d90d12ddafc3c244a
Brian Cortright <brcortright@gmail.com>
2026-06-12 21:26:13 -0700

    Go live

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://actionsportssafetyproject.org",
@@ -7,5 +8,5 @@ export default defineConfig({
   server: {
     port: 4321,
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
